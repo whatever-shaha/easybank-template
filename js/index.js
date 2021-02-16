f9938f56f7258f8196d9c660808a0c0d78f68d31
whatever-shaha <shahasadinov@gmail.com>
2021-02-16 17:18:36 +0500

    window keeps overlay class and navigation list unscrolling is fixed

--- js/index.js
+++ js/index.js
@@ -12,4 +12,13 @@ function animator(){
     list.classList.toggle("animList");
     document.body.classList.toggle('unscrollable');
 }
+//window resize issue fixed
+window.addEventListener('resize', ()=> {
+    if(window.innerWidth > 901){
+        list.classList.remove('animList');
+        go.classList.remove('shadow');
+        menu.classList.remove('burger');
+        document.body.classList.remove('unscrollable');
+    }
+})
 console.log('connected');
