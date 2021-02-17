684863df15733e4815b0716e00cb1753721db876
whatever-shaha <shahasadinov@gmail.com>
2021-02-17 17:21:57 +0500

    article layer is DONE. ps maybe has some bugs

--- js/index.js
+++ js/index.js
@@ -4,7 +4,49 @@ let go = document.querySelector('.overlay');
 let list = document.getElementById('list')
 menu.addEventListener('click',()=>{animator()});
 go.addEventListener('click', ()=>{animator()})
+let myDots = $("#tester");
+$(document).ready(function(){
+    $('.article__grid').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots:true,
+        autoplay:true,
+        pauseOnDotsHover:true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 680,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows:false,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:false,
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        // dotsClass: 'tester'
 
+      });
+  });
 
 function animator(){
     menu.classList.toggle('burger');
